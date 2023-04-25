249b346dd1606ceade1b589870f00aa97eac33bd
MaebaFernandez <maebayorgely@gmail.com>
2023-04-25 19:09:01 -0400

    personal registrado
    Seeders

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,34 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+
     public function run(): void
-    { $this->call(tipoGrupo::class);
+    {   $rol = new Rol();
+        $rol->name = 'admin';
+        $rol->display_name = 'Admin';
+        $rol->enabled = 1;
+        $rol->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->username = 'admin';
+//    $user->email_verified_at = now();
+        $user->enabled = 1;
+        $user->password = Hash::make('123456789');
+        $user->remember_token = Str::random(10);
+        // $user->rol_id = 1;
+        $user->save();
+
+        UserRol::create([
+            "rol_id" => 1,
+            "user_id" => 1
+        ]);
+
+
+
+        $this->call(tipoGrupo::class);
         $this->call(GruposTrabajo::class);
         $this->call(Personal::class);
 
